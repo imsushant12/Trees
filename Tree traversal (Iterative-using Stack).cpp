6981a2a6d10fd991ebf8003e71f67653f7abafaf
Sushant Gaurav <68695162+imsushant12@users.noreply.github.com>
2020-10-20 21:39:20 +0530

    Tree traversal (Iterative-using Stack).cpp

---
+++ Tree traversal (Iterative-using Stack).cpp
@@ -0,0 +1,143 @@
+//by Sushant Gaurav
+
+#include <bits/stdc++.h>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+struct treenode                                                                 //tree
+{
+    int info;
+    struct treenode *left,*right;
+};
+
+
+struct treenode* create()
+{
+    int data;
+    struct treenode *tree;
+    tree=(struct treenode*) malloc (sizeof(struct treenode));
+    printf("\nEnter data to be inserted or type -1 for no insertion : ");
+    scanf("%d",&data);
+
+    if(data==-1)
+        return 0;
+
+    tree->info=data;
+
+    printf("\nEnter left child of %d",data);
+    tree->left=create();
+
+    printf("\nEnter right child of %d",data);
+    tree->right=create();
+
+    return tree;
+};
+
+void preorder(struct treenode *root)
+{
+    stack <treenode*> s;                                              //tree node type stack
+
+    while((root != NULL) || (!s.empty()))
+    {
+        if(root != NULL)
+        {
+            cout << root->info << " ";
+
+            s.push(root);
+
+            root = root->left;
+        }
+        else
+        {
+            root = s.top();
+            s.pop();
+            root = root->right;
+        }
+    }
+    cout<<endl;
+}
+
+
+
+void inorder(struct treenode *root)
+{
+    stack <treenode*> s2;                                                      //stack of tree-node type
+
+    while((root != NULL) || (!s2.empty()))
+    {
+        if(root != NULL)
+        {
+            s2.push(root);
+            root = root->left;
+        }
+        else
+        {
+            root = s2.top();
+            s2.pop();
+            cout << root->info << " ";
+            root = root->right;
+        }
+    }
+    cout<<endl;
+}
+
+
+
+void postorder(struct treenode *root)
+{
+    stack <treenode*> s3;
+
+    struct treenode *previous = NULL;
+
+    do
+    {
+        while(root != NULL)
+        {
+            s3.push(root);
+            root = root->left;
+        }
+
+        while(root == NULL && (!s3.empty()))
+        {
+            root = s3.top();
+            if(root->right == NULL || root->right == previous)
+            {
+                cout<<root->info<<" ";
+                s3.pop();
+                previous = root;
+                root = NULL;
+
+            }
+            else
+            {
+                root = root->right;
+            }
+        }
+
+    }while(!s3.empty());
+
+    cout<<endl;
+
+}
+
+
+
+int main()
+{
+
+    struct treenode *root;
+    root=create();
+
+    cout<<"\nPre-order is : ";
+    preorder(root);
+
+    cout<<"\nIn-order is : ";
+    inorder(root);
+
+    cout<<"\nPost-order is : ";
+    postorder(root);
+
+    return 0;
+}
